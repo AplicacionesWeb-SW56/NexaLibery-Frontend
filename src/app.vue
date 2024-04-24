3b69629b810c1970b333dc32b2281d74bc3eb912
MRamirez202210582 <U202210582@upc.edu.pe>
2024-04-23 19:53:33 -0500

    adding Vue-Project folders

---
+++ src/app.vue
@@ -0,0 +1,18 @@
+<template>
+  <navbar-content></navbar-content>
+  <router-view></router-view>
+  <footer-content/>
+</template>
+
+<script>
+import NavbarContent from "@/public/components/navbar-content.vue";
+import FooterContent from "@/public/components/footer-content.vue";
+
+export default {
+  name: 'App',
+  components: {
+    NavbarContent,
+    FooterContent
+  }
+}
+</script>
